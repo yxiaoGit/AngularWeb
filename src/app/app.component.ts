cba165c7eadd6ada3652a764926ff8ac4068a84a
Ying Xiao <yxabc@yahoo.com>
2024-09-19 10:11:00 -0700

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,31 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `
+        uppercase: {{"Brea" | uppercase }} <br>
+       
+        legend... {{wait | async}} {{dairy | async}}
+
+         Date: {{ today | date:'yMMMMEEEEhmsz'}} <br>
+        Number: {{ 3.141592789 | number: '2.1-5'}} <br>
+        Currency: {{125.257 | currency:'USD':true: '1.2-2'}} <br>
+        Currency: {{125.257 | currency}} <br>
+        Percent: {{.8888 | percent: '2.2'}} <br>
+        Json: {{jsonObject | json}} <br>
+        Slice: {{"hello world" | slice:0:8}} <br>
+
+     `
 })
 export class AppComponent {
+  today= Date.now();
   title = 'builtinPipes';
+  jsonObject = [{title: "mytitle"}, {title: "Programmer"}];
+  wait = new Promise<string> ( (res, err) => {
+      setTimeout( ()=> res("wait for it...'"), 1000);
+  });
+
+  dairy = new Promise<string> ( (res, err) => {
+    setTimeout( ()=> res("dairy.'"), 2000);
+});
+
 }
